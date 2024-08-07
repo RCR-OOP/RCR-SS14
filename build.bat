34cad3f6a5fc4e6377b34a9578697009e8f0f4d5
Romanin <60302782+romanin-rf@users.noreply.github.com>
2024-08-07 16:38:32 +0200

    Обновление перевода

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
-dotnet restore
-dotnet build Content.Packaging --configuration Release --no-restore /m;
-dotnet run --project Content.Packaging server --platform win-x64 --platform linux-x64 --platform osx-x64 --platform linux-arm64;
-dotnet run --project Content.Packaging client --no-wipe-release
+dotnet build Content.Packaging --configuration Release
+dotnet run --project Content.Packaging server --hybrid-acz --platform win-x64 --platform linux-x64 --platform osx-x64 --platform linux-arm64
